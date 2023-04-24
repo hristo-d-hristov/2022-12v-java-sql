f0dc59b43f338aa6c07fa6668889c2664abf625f
Hristo Hristov <hristo.dr.hristov@gmail.com>
2023-04-24 09:02:18 +0300

    add database

--- database.sql
+++ database.sql
@@ -1,2 +1,10 @@
 DROP DATABASE IF EXISTS cars;
 CREATE DATABASE cars;
+USE cars;
+CREATE TABLE potrebiteli (id INT, name VARCHAR(255), mail VARCHAR(255), pass VARCHAR(255));
+
+INSERT INTO potrebiteli ......
+
+CREATE TABLE artikuli(id INT, vid VARCHAR(255), ime VARCHAR(255), snimka VARCHAR(255), cena INT);
+
+INSERT INTO artikuli ........
