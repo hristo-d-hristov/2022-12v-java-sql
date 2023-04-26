25badb969ddefe7c0752ab202e8b8f8e30d9f523
Hristo Hristov <hristo.dr.hristov@gmail.com>
2023-04-26 09:54:25 +0300

    login

--- database.sql
+++ database.sql
@@ -3,8 +3,9 @@ CREATE DATABASE cars;
 USE cars;
 CREATE TABLE potrebiteli (id INT, name VARCHAR(255), mail VARCHAR(255), pass VARCHAR(255));
 
-INSERT INTO potrebiteli ......
+INSERT INTO potrebiteli VALUES (1, "admin", "admin@local.bg", "adm");
+INSERT INTO potrebiteli VALUES (2, "usr", "usr@local.bg", "1234");
 
 CREATE TABLE artikuli(id INT, vid VARCHAR(255), ime VARCHAR(255), snimka VARCHAR(255), cena INT);
 
-INSERT INTO artikuli ........
+--INSERT INTO artikuli ........
--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -4,7 +4,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
@@ -12,14 +18,23 @@ public class LoginServlet extends HttpServlet {
             HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-        PrintWriter writer = response.getWriter();
-        String user = request.getParameter("user");
-        String pass = request.getParameter("pass");
-        if (user.equals("Hristov")
-                && pass.equals("1234")) {
-            writer.println("Successful login");
-        } else {
-            writer.println("Login failed");
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conn  = DriverManager.getConnection("jdbc:mariadb://localhost/cars");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM potrebiteli");
+            conn.close();
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                String n = rs.getString(2);
+                String p = rs.getString(3);
+                response.getWriter().println(id + " " + n + " " + p);
+            }
+   
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            
         }
         
     }
